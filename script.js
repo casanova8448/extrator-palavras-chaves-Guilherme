264bd6b78515483706dac97292023a721f600c60
casanova8448 <guilherme.casanova@escola.pr.gov.br>
2025-10-21 09:25:00 -0300

    21/10

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", MostraPalavrasChave)
+
+function MostraPalavrasChave(){
+    alert("fui clicado!");
+}
