b25bc77be2e7f8ec43f90f39d2472439a7f7fe37
casanova8448 <guilherme.casanova@escola.pr.gov.br>
2025-10-21 10:16:09 -0300

    21/10

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", MostraPalavrasChave);
 
 function MostraPalavrasChave() {
-    const texto = document. querySelector("#entrada-de-texto").Value;
-    const camporesultado = document.querySelector("#resultado-palavrachave");
+    const texto = document.querySelector("#entrada-de-texto").Value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
     const palavras = texto.split(" ");
 
     campoResultado.textContent=palavras.join(", ")
