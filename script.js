8e521a8ef0d74d3e8b3d3c94da8cedc75fe43fd4
casanova8448 <guilherme.casanova@escola.pr.gov.br>
2025-11-11 10:29:42 -0300

    11/11

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { PALAVRAS_RUINS } from "./palavrasRuins.js";
+import { PALAVRAS_RUINS } from "./plavrasRuins.js";
 
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
@@ -14,11 +14,11 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
     for (let i in palavras) {
         palavras[i] = palavras[i].toLowerCase();
     }
-    palavras = tiraPalavrasRuins(palavras)
-
+    palavras = tiraPalavrasRuins(palavras);
 
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavras);
@@ -49,14 +49,14 @@ function contaFrequencias(palavras) {
     return frequencias;
 
 
+
+
+
 }
 
 function tiraPalavrasRuins(palavras) {
 
-
-
     const palavrasBoas = [];
-
 
     for (let palavra of palavras) {
         if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
